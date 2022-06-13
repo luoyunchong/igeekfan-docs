64fe726daed856e68c5997c84c262ee7851fe8c7
luoyunchong <luoyunchong@foxmail.com>
2022-06-13 15:27:51 +0000

    Deploying to gh-pages from @ luoyunchong/igeekfan-docs@5456639ad699b03743a2e8915a14ae93af462081 🚀

---
+++ assets/vue-start.html.bacf6f3c.js
@@ -0,0 +1,54 @@
+import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{r as l,c as t,a as n,b as a,F as i,e,d as o,o as c}from"./app.d57c20c4.js";const p={},u=n("h1",{id:"\u524D\u7AEF\u51C6\u5907",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u524D\u7AEF\u51C6\u5907","aria-hidden":"true"},"#"),e(" \u524D\u7AEF\u51C6\u5907")],-1),d=n("h2",{id:"\u5F00\u6E90\u5730\u5740",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u5F00\u6E90\u5730\u5740","aria-hidden":"true"},"#"),e(" \u5F00\u6E90\u5730\u5740")],-1),b=e("\u7BA1\u7406\u7AEF UI "),h={href:"https://github.com/luoyunchong/lin-cms-vue",target:"_blank",rel:"noopener noreferrer"},m=e("https://github.com/luoyunchong/lin-cms-vue"),_=e("\u7528\u6237\u7AEF UI "),g={href:"https://github.com/luoyunchong/lin-cms-vvlog",target:"_blank",rel:"noopener noreferrer"},v=e("https://github.com/luoyunchong/lin-cms-vvlog"),x=n("h2",{id:"\u535A\u5BA2\u6A21\u5757\u7EBF\u4E0A\u5730\u5740",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u535A\u5BA2\u6A21\u5757\u7EBF\u4E0A\u5730\u5740","aria-hidden":"true"},"#"),e(" \u535A\u5BA2\u6A21\u5757\u7EBF\u4E0A\u5730\u5740")],-1),q=e("\u672C\u9879\u76EE swagger \u5730\u5740 "),f={href:"https://api.igeekfan.cn/swagger/index.html",target:"_blank",rel:"noopener noreferrer"},y=e("https://api.igeekfan.cn/swagger/index.html"),w=e("\u7528\u6237\u7AEF lin-cms-vvlog "),k={href:"https://vvlog.baimocore.cn",target:"_blank",rel:"noopener noreferrer"},j=e("https://vvlog.baimocore.cn"),N=n("ul",null,[n("li",null,"\u666E\u901A\u7528\u6237\uFF1A710277267@qq.com"),n("li",null,"\u5BC6\u7801\uFF1A123qwe")],-1),I=e("\u7BA1\u7406\u5458 lin-cms-vue "),V={href:"https://cms.baimocore.cn",target:"_blank",rel:"noopener noreferrer"},B=e("https://cms.baimocore.cn/"),E=n("ul",null,[n("li",null,"\u7BA1\u7406\u5458\uFF1A admin"),n("li",null,"\u5BC6\u7801\uFF1A123qwe")],-1),U=n("h2",{id:"\u5FEB\u901F\u4E0A\u624B",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u5FEB\u901F\u4E0A\u624B","aria-hidden":"true"},"#"),e(" \u5FEB\u901F\u4E0A\u624B")],-1),F=n("p",null,"\u5F00\u53D1\u5FC5\u5907",-1),L={href:"https://nodejs.org/en/",target:"_blank",rel:"noopener noreferrer"},S=e("Node.js 10+"),$=e(" \u7248\u672C\u5373\u53EF\uFF0C\u6211\u987B\u5B89\u88C5 12.7"),z={href:"https://yarnpkg.com/zh-Hant/docs/install#windows-stable",target:"_blank",rel:"noopener noreferrer"},C=e("yarn"),H=o(`<p>\u5982\u679C\u4EE5\u4E0B\u547D\u4EE4\u6709\u95EE\u9898\uFF0C\u8BF7\u5220\u9664 yarn.lock\uFF0Cnode_modules \u6587\u4EF6\u5939\u540E\uFF0C\u91CD\u65B0\u6267\u884C yarn\uFF0Cyarn serve</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># clone the project
+git clone https://github.com/luoyunchong/lin-cms-vue.git
+
+# install dependency
+yarn
+
+# develop
+yarn serve
+
+# deploy
+yarn deploy
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br></div></div><h2 id="deploy-\u53D1\u5E03" tabindex="-1"><a class="header-anchor" href="#deploy-\u53D1\u5E03" aria-hidden="true">#</a> deploy \u53D1\u5E03</h2><p>scp2 \u65B9\u4FBF\u5FEB\u901F\u53D1\u5E03\uFF0C\u4E00\u884C\u547D\u4EE4\u5C31\u80FD\u5FEB\u901F\u53D1\u5E03\u6210\u529F\u3002</p><p>\u5FC5\u5907\u6761\u4EF6\uFF1A\uFF08\u53C2\u6570\uFF09</p><ul><li>\u4E00\u53F0 linux \u7684\u670D\u52A1\u5668\uFF0Cip</li><li>\u7528\u6237\u540D</li><li>\u5BC6\u7801</li><li>\u7AEF\u53E3\uFF1A\u9ED8\u8BA4\u662F 22</li><li>\u53D1\u5E03\u7684\u5730\u5740\u3002\u8FD9\u91CC\u653E\u5230/var/www/lin-cms-vue \u76EE\u5F55\u4E2D\u3002</li></ul><h2 id="\u6B65\u9AA4" tabindex="-1"><a class="header-anchor" href="#\u6B65\u9AA4" aria-hidden="true">#</a> \u6B65\u9AA4</h2><p>\u6839\u76EE\u5F55\u65B0\u5EFA deploy \u76EE\u5F55\uFF0C\u521B\u5EFA index.js \u6587\u4EF6\u3002</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&#39;use strict&#39;
+// \u5F15\u5165scp2\u6A21\u5757
+var client = require(&#39;scp2&#39;);
+const ora = require(&#39;ora&#39;)
+const chalk = require(&#39;chalk&#39;)
+const spinner = ora(&#39;\u6B63\u5728\u53D1\u5E03\u5230\u751F\u4EA7\u670D\u52A1\u5668...&#39;)
+spinner.start()
+client.scp(&#39;dist/&#39;, {
+    &quot;host&quot;: &quot;&quot;,
+    &quot;username&quot;: &quot;&quot;,
+    &quot;password&quot;: &quot;&quot;,
+    &quot;port&quot;: &quot;22&quot;,
+    &quot;path&quot;: &quot;/var/www/lin-cms-vue&quot;
+}, function (err) {
+    spinner.stop()
+    if (!err) {
+        console.log(&quot;npm run build-scp2: scp2\u5DE5\u5177\u4E0A\u4F20\u5B8C\u6BD5,\u8FDC\u7AEF\u670D\u52A1\u8DEF\u5F84\uFF1A/var/www/lin-cms-vue&quot;)
+    } else {
+        console.log(&quot;err&quot;, err)
+    }
+})
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br></div></div><p>\u5FEB\u901F\u53D1\u5E03\uFF0C\u9700\u8981\u5B89\u88C5 scp2</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>cnpm install scp2
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br></div></div><p>package.json \u4E2D\u589E\u52A0</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>  &quot;scripts&quot;: {
+    &quot;deploy&quot;: &quot;yarn build:production &amp;&amp; node ./deploy&quot;,
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br></div></div><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>yarn deploy
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br></div></div><h2 id="nginx-\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#nginx-\u914D\u7F6E" aria-hidden="true">#</a> nginx \u914D\u7F6E</h2><ul><li>vue \u4F7F\u7528 history \u7684\u914D\u7F6E</li></ul><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>server {
+    listen 8080;
+    root /var/www/lin-cms-vue;
+
+   charset utf-8;
+   location / {
+       try_files $uri $uri/ /index.html;
+
+     }
+
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   html;
+    }
+
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br></div></div><h2 id="\u914D\u7F6E\u9879" tabindex="-1"><a class="header-anchor" href="#\u914D\u7F6E\u9879" aria-hidden="true">#</a> \u914D\u7F6E\u9879</h2><ol><li><p>\u914D\u7F6E api \u5730\u5740\uFF1A \u6253\u5F00\u914D\u7F6E\u6587\u4EF6 src/config/index.js \u914D\u7F6E baseUrl \uFF0C\u672C\u5730\u5F00\u53D1\u9636\u6BB5\u914D\u7F6E\u672C\u5730\u865A\u62DF\u57DF\u540D(https://localhost:5001/)\uFF0C\u7EBF\u4E0A\u90E8\u7F72\u751F\u4EA7\u57DF\u540D\u3002</p></li><li><p>\u7528\u6237\u540D\uFF1A<strong>admin</strong> \u5BC6\u7801 <strong>123qwe</strong></p></li></ol>`,19);function T(A,D){const s=l("ExternalLinkIcon");return c(),t(i,null,[u,d,n("ul",null,[n("li",null,[b,n("a",h,[m,a(s)])]),n("li",null,[_,n("a",g,[v,a(s)])])]),x,n("ul",null,[n("li",null,[q,n("a",f,[y,a(s)])]),n("li",null,[w,n("a",k,[j,a(s)]),N]),n("li",null,[I,n("a",V,[B,a(s)]),E])]),U,F,n("ul",null,[n("li",null,[n("a",L,[S,a(s)]),$]),n("li",null,[n("a",z,[C,a(s)])])]),H],64)}var K=r(p,[["render",T],["__file","vue-start.html.vue"]]);export{K as default};
