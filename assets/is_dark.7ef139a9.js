a18e95396869fe61fada9555a572eb7e41b7a5f3
luoyunchong <luoyunchong@foxmail.com>
2024-01-07 06:28:56 +0000

    Deploying to gh-pages from @ luoyunchong/igeekfan-docs@56de788c361a2672efcbfe788d56e6dd63b3058d 🚀

---
+++ assets/is_dark.7ef139a9.js
@@ -0,0 +1,1 @@
+import{C as o,_ as r}from"./mermaid.core.9f3ef4c5.js";const s=n=>{const{r:a,g:t,b:e}=o.parse(n),i=.2126*r.channel.toLinear(a)+.7152*r.channel.toLinear(t)+.0722*r.channel.toLinear(e);return r.lang.round(i)};var c=s;const l=n=>c(n)>=.5;var u=l;const h=n=>!u(n);var L=h;export{L as i};
